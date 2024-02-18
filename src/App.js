7ee4952fc217e2d23af040cfa67ea5d3f9f8c5b6
wakeupmakeup <zerostar998@naver.com>
2024-02-18 17:55:28 +0900

    여섯번째 커밋 - Update 추가

--- src/App.js
+++ src/App.js
@@ -51,6 +51,7 @@ function Article(props) {
 
 // Create라는 이름의 함수형 컴포넌트를 정의합니다. 이 컴포넌트는 새로운 항목을 생성하는 폼을 렌더링합니다.
 function Create(props) {
+
   // 폼을 제출하면 기본 동작인 페이지 리로드를 막고, 입력 필드에서 값을 가져와서 props로 받은 onCreate 함수를 호출합니다.
   // 이 함수를 통해 App 컴포넌트의 topics 상태가 변경됩니다.
   return <article>
@@ -61,12 +62,36 @@ function Create(props) {
       const body = event.target.body.value;
       props.onCreate(title, body);
     }}>
-      <p><input type="text" name="title" placeholder='title' /></p>
-      <p><textarea name="body" placeholder='body'></textarea></p>
+      <p><input type="text" name="title" placeholder='title' value={title} /></p>
+      <p><textarea name="body" placeholder='body' value={body}></textarea></p>
       <p><input type="submit" value="Create"></input></p>
     </form>
   </article>
 }
+
+function Update(props) {
+  // props로 들어온 데이터를 state로 환승했다.
+  const [title, setTitle] = useState(props, title);
+  const [body, setBody] = useState(props, body);
+  return <article>
+    <h2>Update</h2>
+    <form onSubmit={event => {
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onUpdate(title, body);
+    }}>
+      <p><input type="text" name="title" placeholder='title' onChange={event => {
+        setTitle(event.target.value);
+      }} /></p>
+      <p><textarea name="body" placeholder='body' onChange={event => {
+        setBody(event.target.value);
+      }}></textarea></p>
+      <p><input type="submit" value="Update"></input></p>
+    </form>
+  </article>
+}
+
 
 // App이라는 이름의 함수형 컴포넌트를 정의합니다. 이 컴포넌트는 전체 앱의 상태를 관리하고, 다른 컴포넌트들을 조합하여 전체 앱을 렌더링합니다. 
 function App() {
@@ -81,6 +106,7 @@ function App() {
   ])
 
   let content = null;
+  let contextControl = null;
 
   // mode의 값에 따라서 content를 결정합니다.
   if (mode === 'WELCOME') {
@@ -95,7 +121,16 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>
-  } else if (mode === 'CREATE') {
+    contextControl = <li><a href={"/update/" + id} onClick={event => {
+      event.preventDefault();
+      setMode('UPDATE'); // mode 가 update일 경우 뜨게 하면 된다. 
+    }}>Update</a></li>
+    // mode => read 인 경우에만 이게 뜨게 만든다. 
+    // id를 저렇게 추가해서 형식을 지켜주자.
+
+  }
+
+  else if (mode === 'CREATE') {
     // mode가 'CREATE'일 때, Create 컴포넌트를 렌더링합니다.
     content = <Create onCreate={(_title, _body) => {
       const newTopic = { id: nextId, title: _title, body: _body }
@@ -106,6 +141,26 @@ function App() {
       setId(nextId);
       setNextId(nextId + 1);
     }}></Create>
+  } else if (mode === 'UPDATE') {
+    let title, body = null;
+
+    for (let i = 0; i < topics.length; i++) {
+      if (topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Update title={title} body={body} onUpdate={(title, body) => {
+      const newTopics = [...topics]
+      const updatedTopic = { id: id, title: title, body: body } // 수정된 토픽 만들기
+      for (let i = 0; i < newTopics.length; i++) {
+        if (newTopics[i].id === id) {
+          newTopics[i] = updatedTopic;
+          break;
+        }
+      }
+      setTopics(newTopics);
+    }}></Update>
   }
 
   // App 컴포넌트의 렌더링 결과입니다. Header, Nav, content, 그리고 'Create' 링크를 렌더링합니다.
@@ -119,11 +174,19 @@ function App() {
         setId(_id);
       }}></Nav>
       {content}
-      <a href="/create" onClick={event => {
-        event.preventDefault();
-        setMode('CREATE');
-      }}>Create</a>
+      <ul>
+        <li><a href="/create" onClick={event => {
+          event.preventDefault();
+          setMode('CREATE');
+        }}>Create</a></li>
+        {contextControl}
+
+      </ul>
     </div>
+    // update 기능은 상세 페이지에 들어가야 보여지는게 세련되어 보인다. 
+    // 그럼 어떻게 할까? 
+    // 위에 read모드 함수를 보면 답이 있다. -> 사용자가 홈으로 갔을때는 안보이다가
+    // read모드로 가는 순간 보이게 했다. 
   );
 }
 
